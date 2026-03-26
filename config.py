3d3ea27d967ac8e93953f91db6b3154d158ec42b
claudioperr059-jpg <claudioperr059@gmail.com>
2026-03-26 17:26:36 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+"""
+CONFIGURAZIONE DEL BOT
+Sostituisci con i tuoi dati
+"""
+
+# Configurazione Telegram
+TELEGRAM_TOKEN = "IL_TUO_TOKEN_DA_BOTFATHER"
+TELEGRAM_CHAT_ID = "IL_TUO_CHAT_ID"
+
+# Orario invio automatico (24h)
+ORARIO_INVIO = "10:00"
+
+# Soglia per auto-apprendimento
+SOGLIA_APPRENDIMENTO = 3
+
+# Campionati da escludere
+CAMPIONATI_ESOTICI = [
+    'India', 'Cina', 'Serbia', 'Egitto', 'Perù', 
+    'Vietnam', 'Indonesia', 'Romania', 'Colombia', 'Argentina'
+]
